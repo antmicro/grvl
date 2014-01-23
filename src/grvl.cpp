d57f55bdb72a35ecff19fbc7780fc4c7ae0e572b
Peter Gielda <pgielda@antmicro.com>
2014-01-23 09:14:35 +0100

    Added callbacks injection utility (injecting custom malloc, free, dma ops, printf, mutex locks)
    This adds an "init" method for library where user provides custom
    callbacks for a few os-specific/device specific methods. The injectible methods are:
    - memory management (malloc, free)
    - mutex operations (lock, unlock)
    - time fetching (get_timestamp)
    - printf
    - drawing related (dma ops, layer pointer setting, waiting for vsync, flipping buffers
      completed callback)
    
    Co-authored-by: Michael Gielda <mgielda@antmicro.com>
    Co-authored-by: Piotr Zierhoffer <pzierhoffer@antmicro.com>
    Co-authored-by: Mateusz Holenko <mholenko@antmicro.com>
    Co-authored-by: Wojciech Strozynski <wstrozynski@antmicro.com>

---
+++ src/grvl.cpp
@@ -0,0 +1,56 @@
+// Copyright 2014-2024 Antmicro <antmicro.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "grvl.h"
+
+namespace grvl {
+
+    static gui_callbacks_t callbacks;
+
+    void grvl::Init(gui_callbacks_t* n_callbacks)
+    {
+        if(n_callbacks) {
+            memcpy(&callbacks, n_callbacks, sizeof(callbacks));
+        } else {
+            callbacks.free = free;
+            callbacks.malloc = malloc;
+            callbacks.dma_operation = NULL;
+            callbacks.gui_printf = NULL;
+            callbacks.mutex_create = NULL;
+            callbacks.mutex_lock = NULL;
+            callbacks.mutex_unlock = NULL;
+            callbacks.mutex_destroy = NULL;
+            callbacks.get_timestamp = NULL;
+        }
+    }
+
+    gui_callbacks_t* grvl::Callbacks()
+    {
+        return &callbacks;
+    }
+
+    void grvl::Log(const char* text, ...)
+    {
+        if(grvl::Callbacks()->gui_printf == 0) {
+            return;
+        }
+        va_list argList;
+        va_start(argList, text);
+        grvl::Callbacks()->gui_printf(text, argList);
+        va_end(argList);
+    }
+
+} /* namespace grvl */
