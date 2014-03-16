b2747e982934c9aea3de8c33e43c8928cb1ce2ef
Peter Gielda <pgielda@antmicro.com>
2014-03-16 09:23:08 +0100

    Added clock widget displaying current time
    The clock widget takes time information from time.h
    and displays it on screen (like a label, but with predefined text).
    
    Co-authored-by: Michael Gielda <mgielda@antmicro.com>
    Co-authored-by: Piotr Zierhoffer <pzierhoffer@antmicro.com>
    Co-authored-by: Mateusz Holenko <mholenko@antmicro.com>
    Co-authored-by: Wojciech Strozynski <wstrozynski@antmicro.com>

---
+++ include/Clock.h
@@ -0,0 +1,106 @@
+// Copyright 2014-2024 Antmicro <antmicro.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef GRVL_CLOCK_H_
+#define GRVL_CLOCK_H_
+
+#include "stl.h"
+#include <time.h>
+
+#include "Font.h"
+#include "Label.h"
+#include "tinyxml2.h"
+
+using namespace tinyxml2;
+
+namespace grvl {
+    class Manager;
+
+    /// Widget displaying current time.
+    ///
+    /// XML parameters:
+    /// * id                    - widget identifier
+    /// * x                     - widget position on x axis in pixels
+    /// * y                     - widget position on y axis in pixels
+    /// * width                 - widget width in pixels
+    /// * height                - widget height in pixels
+    /// * visible               - indicates if the widget is visible
+    ///
+    /// * foregroundColor       - text color (default: black)
+    /// * backgroundColor       - background color (default: transparent)
+    /// * font                  - text font (default: normal)
+    /// * alignment             - horizontal text alignment (default: centered)
+    ///
+    /// XML events:
+    /// * onClick               - event invoked when touch is released, but only when it has not left widget boundaries
+    ///                           since pressing
+    /// * onPress               - event invoked when touch is detected within widget boundaries
+    /// * onRelease             - event invoked when touch is released within widget boundaries
+    ///                           or when it leaves the boundaries
+    ///
+    class Clock : public Label {
+    public:
+        Clock()
+            : Label()
+            , isRunning(false)
+            , lastCurrentTime(0)
+        {
+        }
+
+        Clock(int32_t x, int32_t y, int32_t width, int32_t height)
+            : Label(x, y, width, height)
+            , isRunning(false)
+            , lastCurrentTime(0)
+        {
+        }
+
+        Clock(int32_t x, int32_t y, int32_t width, int32_t height, TextHorizontalAlignment alignment)
+            : Label(x, y, width, height, "", alignment)
+            , isRunning(false)
+            , lastCurrentTime(0)
+        {
+        }
+
+        Clock(const Clock& Obj)
+            : Label(Obj)
+            , isRunning(Obj.isRunning)
+            , lastCurrentTime(Obj.lastCurrentTime)
+        {
+        }
+
+        Clock& operator=(const Clock& Obj);
+
+        virtual ~Clock();
+
+        static Clock* BuildFromXML(XMLElement* xmlElement);
+
+        virtual void Draw(Painter& painter, int32_t ParentX, int32_t ParentY, int32_t ParentWidth, int32_t ParentHeight);
+        void SetVisibleSeconds(bool value) { visibleSeconds = value; }
+
+        void Start();
+        void Stop();
+        bool IsRunning() const;
+
+    private:
+        bool isRunning;
+        bool visibleSeconds;
+        time_t lastCurrentTime;
+        static const int bufferSize = 10;
+    };
+
+} /* namespace grvl */
+
+#endif /* GRVL_CLOCK_H_ */
---
+++ src/Clock.cpp
@@ -0,0 +1,111 @@
+// Copyright 2014-2024 Antmicro <antmicro.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "Clock.h"
+#include "Manager.h"
+#include "XMLSupport.h"
+
+namespace grvl {
+
+    Clock::~Clock()
+    {
+    }
+
+    void Clock::Start()
+    {
+        isRunning = true;
+    }
+
+    void Clock::Stop()
+    {
+        isRunning = false;
+    }
+
+    bool Clock::IsRunning() const
+    {
+        return isRunning;
+    }
+
+    Clock* Clock::BuildFromXML(XMLElement* xmlElement)
+    {
+        Manager* man = &Manager::GetInstance();
+
+        int32_t x;
+        int32_t y;
+        int32_t height;
+        int32_t width;
+        if(!XMLSupport::TryGetIntAttribute(xmlElement, "x", &x)
+           || !XMLSupport::TryGetIntAttribute(xmlElement, "y", &y)
+           || !XMLSupport::TryGetIntAttribute(xmlElement, "width", &width)
+           || !XMLSupport::TryGetIntAttribute(xmlElement, "height", &height)) {
+            return NULL;
+        }
+
+        Clock* result = new Clock(x, y, width, height);
+        const char* id = xmlElement->Attribute("id");
+        if(id != NULL) {
+            result->SetID(id);
+        }
+        result->SetText("");
+        static constexpr uint32_t defaultFg = 0xFFFFFFFF; /*white*/
+        result->SetForegroundColor(XMLSupport::ParseColor(xmlElement, "foregroundColor", defaultFg));
+        result->SetBackgroundColor(XMLSupport::GetAttributeOrDefault(xmlElement, "backgroundColor", (uint32_t)COLOR_ARGB8888_TRANSPARENT));
+
+        result->SetTextFont(man->GetFontPointer(XMLSupport::GetAttributeOrDefault(xmlElement, "font", "normal")));
+        result->SetHorizontalAlignment(
+            XMLSupport::ParseAlignmentOrDefault(xmlElement, "alignment", Label::Center));
+        result->SetVisible(XMLSupport::GetAttributeOrDefault(xmlElement, "visible", true));
+        result->SetVisibleSeconds(XMLSupport::GetAttributeOrDefault(xmlElement, "seconds", false));
+        result->SetOnClickEvent(man->GetEventWithArguments(xmlElement->Attribute("onClick")));
+        result->SetOnReleaseEvent(man->GetEventWithArguments(xmlElement->Attribute("onRelease")));
+        result->SetOnPressEvent(man->GetEventWithArguments(xmlElement->Attribute("onPress")));
+
+        result->Start();
+
+        return result;
+    }
+
+    void Clock::Draw(Painter& painter, int32_t ParentX, int32_t ParentY, int32_t ParentWidth, int32_t ParentHeight)
+    {
+
+        if(!Visible) {
+            return;
+        }
+
+        if(isRunning) {
+            time_t current_time;
+            current_time = time(NULL);
+            if(lastCurrentTime != current_time) {
+                char buf[bufferSize];
+                strftime(buf, bufferSize, visibleSeconds ? "%H:%M:%S" : "%H:%M", localtime(&current_time));
+                SetText(buf);
+                lastCurrentTime = current_time;
+            }
+        }
+        Label::Draw(painter, ParentX, ParentY, ParentWidth, ParentHeight);
+    }
+
+    Clock& Clock::operator=(const Clock& Obj)
+    {
+        if(this != &Obj) {
+            Label::operator=(Obj);
+            isRunning = Obj.isRunning;
+            lastCurrentTime = Obj.lastCurrentTime;
+        }
+        return *this;
+    }
+
+} /* namespace grvl */
