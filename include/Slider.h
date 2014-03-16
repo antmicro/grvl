07cee836a50cc34f59636a817b13832f35fa8150
Peter Gielda <pgielda@antmicro.com>
2014-03-16 09:22:50 +0100

    Added slider component (horizontal scroll)
    The widget is a slider with customizable scrolling element etc.
    It also has onValueChange event handling aside from the regular events.
    
    Co-authored-by: Michael Gielda <mgielda@antmicro.com>
    Co-authored-by: Piotr Zierhoffer <pzierhoffer@antmicro.com>
    Co-authored-by: Mateusz Holenko <mholenko@antmicro.com>
    Co-authored-by: Wojciech Strozynski <wstrozynski@antmicro.com>

---
+++ include/Slider.h
@@ -0,0 +1,218 @@
+// Copyright 2014-2024 Antmicro <antmicro.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef GRVL_SLIDER_H_
+#define GRVL_SLIDER_H_
+
+#include "Component.h"
+#include "Image.h"
+#include "Painter.h"
+#include "XMLSupport.h"
+#include "tinyxml2.h"
+
+using namespace tinyxml2;
+
+namespace grvl {
+    class Manager;
+
+    /// Horizontal scroll bar widget.
+    ///
+    /// XML parameters:
+    /// * id                      - widget identifier
+    /// * x                       - widget position on x axis in pixels
+    /// * y                       - widget position on y axis in pixels
+    /// * width                   - widget width in pixels
+    /// * height                  - widget height in pixels
+    /// * visible                 - indicates if the widget is visible (default: true)
+    ///
+    /// * backgroundColor         - background color (default: transparent)
+    /// * activeBackgroundColor   - background color when pressed (default: backgroundColor)
+    /// * frameColor              - frame color (default: transparent)
+    /// * selectedFrameColor      - frame color when pressed (default: frameColor)
+    /// * barColor                - scroll bar background color (default: white)
+    /// * activeBarColor          - scroll bar background color when pressed (default: barColor)
+    /// * scrollColor             - scrolling element color (default: blue)
+    /// * activeScrollColor       - scrolling element color when pressed (default: scrollColor)
+    ///
+    /// * minValue                - minimal value (the one on the left) (default: 0)
+    /// * maxValue                - maximal value (the one on the right) (default: 100)
+    ///
+    /// * image                   - identifier of image content replacing standard scrolling element
+    ///
+    /// XML events:
+    /// * onClick                 - event invoked when touch is released, but only when it has not left the boundaries of widget since pressing
+    /// * onPress                 - event invoked when touch is detected in the boundaries of the widget
+    /// * onRelease               - event invoked when touch is released
+    /// * onValueChange           - event invoked when the scrolling element is moved to another value
+    ///
+    class Slider : public Component {
+    public:
+        Slider()
+            : Component()
+            , BarColor(COLOR_ARGB8888_LIGHTGRAY)
+            , ActiveBarColor(COLOR_ARGB8888_GRAY)
+            , ScrollColor(COLOR_ARGB8888_LIGHTBLUE)
+            , ActiveScrollColor(COLOR_ARGB8888_BLUE)
+            , FrameColor(COLOR_ARGB8888_BLACK)
+            , SelectedFrameColor(COLOR_ARGB8888_BLACK)
+            , ScrollLength(0)
+            , MinValue(0)
+            , MaxValue(0)
+            , Position(0.0)
+            , Value(0)
+            , ReportedValue(0)
+            , ScrollImage()
+            , onValueChange()
+            , PreviousValueUpdateTimestamp(0)
+            , KeepBoundaries(false)
+        {
+        }
+
+        Slider(int32_t x, int32_t y, int32_t width, int32_t height)
+            : Component(x, y, width, height)
+            , BarColor(COLOR_ARGB8888_LIGHTGRAY)
+            , ActiveBarColor(COLOR_ARGB8888_GRAY)
+            , ScrollColor(COLOR_ARGB8888_LIGHTBLUE)
+            , ActiveScrollColor(COLOR_ARGB8888_BLUE)
+            , FrameColor(COLOR_ARGB8888_BLACK)
+            , SelectedFrameColor(COLOR_ARGB8888_BLACK)
+            , ScrollLength(0)
+            , MinValue(0)
+            , MaxValue(0)
+            , Position(0.0)
+            , Value(0)
+            , ReportedValue(0)
+            , ScrollImage()
+            , onValueChange()
+            , PreviousValueUpdateTimestamp(0)
+            , KeepBoundaries(false)
+        {
+        }
+
+        Slider(int32_t x, int32_t y, int32_t width, int32_t height, int32_t min, int32_t max)
+            : Component(x, y, width, height)
+            , BarColor(COLOR_ARGB8888_LIGHTGRAY)
+            , ActiveBarColor(COLOR_ARGB8888_GRAY)
+            , ScrollColor(COLOR_ARGB8888_LIGHTBLUE)
+            , ActiveScrollColor(COLOR_ARGB8888_BLUE)
+            , FrameColor(COLOR_ARGB8888_BLACK)
+            , SelectedFrameColor(COLOR_ARGB8888_BLACK)
+            , ScrollLength(0)
+            , MinValue(min)
+            , MaxValue(max)
+            , Position(0.0)
+            , Value(0)
+            , ReportedValue(0)
+            , ScrollImage()
+            , onValueChange()
+            , PreviousValueUpdateTimestamp(0)
+            , KeepBoundaries(false)
+        {
+        }
+
+        Slider(const Slider& Obj)
+            : Component(Obj)
+            , BarColor(Obj.BarColor)
+            , ActiveBarColor(COLOR_ARGB8888_GRAY)
+            , ScrollColor(Obj.ScrollColor)
+            , ActiveScrollColor(Obj.ActiveScrollColor)
+            , FrameColor(Obj.FrameColor)
+            , SelectedFrameColor(Obj.SelectedFrameColor)
+            , ScrollLength(Obj.ScrollLength)
+            , MinValue(Obj.MinValue)
+            , MaxValue(Obj.MaxValue)
+            , Position(Obj.Position)
+            , Value(Obj.Value)
+            , ReportedValue(Obj.ReportedValue)
+            , ScrollImage(Obj.ScrollImage)
+            , onValueChange(Obj.onValueChange)
+            , PreviousValueUpdateTimestamp(Obj.PreviousValueUpdateTimestamp)
+            , KeepBoundaries(Obj.KeepBoundaries)
+        {
+        }
+
+        virtual ~Slider();
+
+        Slider& operator=(const Slider& Obj);
+
+        void SetScrollImage(const Image& image);
+        Image* GetScrollImagePointer();
+
+        void SetBarColor(uint32_t color);
+        void SetActiveBarColor(uint32_t color);
+        void SetScrollColor(uint32_t color);
+        void SetActiveScrollColor(uint32_t color);
+        void SetFrameColor(uint32_t color);
+        void SetSelectedFrameColor(uint32_t color);
+        void SetMinValue(float value);
+        void SetMaxValue(float value);
+        void SetValue(double value);
+        void SetKeepBoundaries(bool value);
+        void SetDiscrete(uint8_t value);
+        void SetDivision(uint8_t value);
+        void CalculateStep();
+        void SetTextFont(Font const* font);
+        void SetLimiters(float const array[], uint8_t size);
+        static string GetValueOnButton(float val);
+
+        uint32_t GetBarColor() const;
+        uint32_t GetActiveBarColor() const;
+        uint32_t GetScrollColor() const;
+        uint32_t GetActiveScrollColor() const;
+        uint32_t GetFrameColor() const;
+        uint32_t GetSelectedFrameColor() const;
+        int32_t GetMinValue() const;
+        int32_t GetMaxValue() const;
+        /// @return Scroll bar's current position.
+        float GetValue() const;
+        bool GetKeepBoundaries() const;
+
+        virtual void OnPress();
+        virtual void OnRelease();
+        virtual void OnClick();
+
+        void SetOnValueChangeEvent(const Event& event);
+
+        virtual Touch::TouchResponse ProcessMove(int32_t StartX, int32_t StartY, int32_t DeltaX, int32_t DeltaY);
+        virtual bool IsTouchPointInObject(int32_t x, int32_t y, int32_t modificator);
+
+        static Slider* BuildFromXML(XMLElement* xmlElement);
+
+        virtual void Draw(Painter& painter, int32_t ParentX, int32_t ParentY, int32_t ParentWidth, int32_t ParentHeight);
+
+    protected:
+        uint32_t BarColor, ActiveBarColor, ScrollColor, ActiveScrollColor, FrameColor, SelectedFrameColor, ScrollLength;
+        uint8_t discrete, division;
+        uint32_t step;
+        float MinValue, MaxValue;
+        float Position, Value, ReportedValue;
+        Image ScrollImage;
+        Event onValueChange;
+        uint64_t PreviousValueUpdateTimestamp;
+        bool KeepBoundaries;
+
+        Font const* SliderFont;
+
+        float* limiters;
+        uint8_t NumberOfLimiters;
+
+        float ValueToPosition(float value) const;
+        float PositionToValue(float position) const;
+    };
+
+} /* namespace grvl */
+
+#endif /* GRVL_SLIDER_H_ */
---
+++ src/Slider.cpp
@@ -0,0 +1,543 @@
+// Copyright 2014-2024 Antmicro <antmicro.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "Slider.h"
+#include "grvl.h"
+#include "Manager.h"
+
+//NOLINTBEGIN(readability-magic-numbers, bugprone-branch-clone, bugprone-integer-division)
+
+namespace grvl {
+
+    Slider::~Slider()
+    {
+        delete[] limiters;
+    }
+
+    void Slider::SetScrollImage(const Image& image)
+    {
+        ScrollImage = image;
+    }
+
+    Image* Slider::GetScrollImagePointer()
+    {
+        return &ScrollImage;
+    }
+
+    void Slider::SetBarColor(uint32_t color)
+    {
+        BarColor = color;
+    }
+
+    void Slider::SetScrollColor(uint32_t color)
+    {
+        ScrollColor = color;
+    }
+
+    void Slider::SetActiveScrollColor(uint32_t color)
+    {
+        ActiveScrollColor = color;
+    }
+
+    void Slider::SetFrameColor(uint32_t color)
+    {
+        FrameColor = color;
+    }
+
+    void Slider::SetMinValue(float value)
+    {
+        Value = value;
+        MinValue = value;
+    }
+
+    void Slider::SetMaxValue(float value)
+    {
+        MaxValue = value;
+    }
+
+    void Slider::SetValue(double value)
+    {
+        if(value >= MinValue && value <= MaxValue) {
+            Value = value;
+            Position = ValueToPosition(Value);
+        }
+    }
+
+    void Slider::SetDiscrete(uint8_t value)
+    {
+        discrete = value;
+    }
+
+    void Slider::SetDivision(uint8_t value)
+    {
+        division = value;
+        CalculateStep();
+    }
+
+    void Slider::CalculateStep()
+    {
+        step = (MaxValue - MinValue) / division;
+    }
+
+    void Slider::SetTextFont(Font const* font)
+    {
+        SliderFont = font;
+    }
+
+    void Slider::SetLimiters(float const array[], uint8_t size)
+    {
+        NumberOfLimiters = size;
+        limiters = new float[NumberOfLimiters];
+        for(uint8_t i = 0; i < NumberOfLimiters; i++)
+            limiters[i] = array[i];
+        MinValue = 0;
+        MaxValue = size - 1.0;
+        discrete = 2;
+    }
+
+    uint32_t Slider::GetBarColor() const
+    {
+        return BarColor;
+    }
+
+    uint32_t Slider::GetScrollColor() const
+    {
+        return ScrollColor;
+    }
+
+    uint32_t Slider::GetActiveScrollColor() const
+    {
+        return ActiveScrollColor;
+    }
+
+    uint32_t Slider::GetFrameColor() const
+    {
+        return FrameColor;
+    }
+
+    int32_t Slider::GetMinValue() const
+    {
+        return MinValue;
+    }
+
+    int32_t Slider::GetMaxValue() const
+    {
+        return MaxValue;
+    }
+
+    float Slider::GetValue() const
+    {
+        return Value;
+    }
+
+#define EPSILON 2.2204460492503131e-16
+
+    bool AreSame(float a, float b)
+    {
+        return fabs(a - b) < EPSILON;
+    }
+
+    string GetFriction(float val)
+    {
+        if(AreSame(4.0, val))
+            return "4";
+        if(AreSame(3.0, val))
+            return "3";
+        if(AreSame(2.0, val))
+            return "2";
+        if(AreSame(1.3, val))
+            return "4/3";
+        if(AreSame(1.0, val))
+            return "1";
+        if(AreSame(0.66, val))
+            return "2/3";
+        if(AreSame(0.33, val))
+            return "1/3";
+        if(AreSame(16.0, val))
+            return "16";
+        if(AreSame(14.0, val))
+            return "14";
+        if(AreSame(12.0, val))
+            return "12";
+        if(AreSame(10.0, val))
+            return "10";
+        if(AreSame(8.0, val))
+            return "8";
+        if(AreSame(6.0, val))
+            return "6";
+        if(AreSame(4.0, val))
+            return "4";
+        if(AreSame(3.0, val))
+            return "3";
+        if(AreSame(2.0, val))
+            return "2";
+        if(AreSame(0.001, val))
+            return "1/1000";
+        if(AreSame(0.002, val))
+            return "1/500";
+        if(AreSame(0.004, val))
+            return "1/250";
+        if(AreSame(0.008, val))
+            return "1/125";
+        if(AreSame(0.016, val))
+            return "1/60";
+        if(AreSame(0.033, val))
+            return "1/30";
+        if(AreSame(0.066, val))
+            return "1/15";
+        if(AreSame(0.125, val))
+            return "1/8";
+        if(AreSame(0.25, val))
+            return "1/4";
+        if(AreSame(0.5, val))
+            return "1/2";
+        return "0";
+    }
+
+    string Slider::GetValueOnButton(float val)
+    {
+        return GetFriction(val);
+    }
+
+    void Slider::Draw(Painter& painter, int32_t ParentX, int32_t ParentY, int32_t ParentWidth, int32_t ParentHeight)
+    {
+        if(!Visible) {
+            return;
+        }
+
+        if(discrete == 0) {
+            painter.FillRectangle(ParentX + X, ParentY + Y, Width, Height, BarColor);
+
+            if(ScrollImage.IsEmpty()) {
+
+                if(Width > Height) {
+                    painter.FillRectangle(
+                        ParentX + X + (Width - ScrollLength) * Position, ParentY + Y, Width * (1.0 - Position), Height,
+                        BarColor);
+                    painter.FillRectangle(ParentX + X, ParentY + Y, Width * Position, Height, ActiveBarColor);
+
+                    ScrollLength = (uint32_t)(Width / 10);
+                    painter.FillRectangle(
+                        ParentX + X + ((Width - ScrollLength) * Position), ParentY + Y, ScrollLength, Height, ScrollColor);
+                } else {
+                    painter.FillRectangle(
+                        ParentX + X, ParentY + Y + Height * (1.0 - Position), Width, ceil(Height * Position),
+                        ActiveBarColor);
+                    painter.FillRectangle(ParentX + X, ParentY + Y, Width, Height * (1.0 - Position), BarColor);
+
+                    ScrollLength = (uint32_t)(Height / 10);
+                    painter.FillRectangle(
+                        ParentX + X, ParentY + Y + ((Height - ScrollLength) * (1.0 - Position)), Width, ScrollLength,
+                        ScrollColor);
+                }
+                // Frame
+                painter.DrawRectangle(ParentX + X, ParentY + Y, Width, Height, FrameColor);
+            } else { // With scroll ico
+                if(Width > Height) {
+                    painter.FillRectangle(ParentX + X + (Width * Position), ParentY + Y, Width * (1.0 - Position), Height,
+                                          BarColor);
+                    painter.FillRectangle(ParentX + X, ParentY + Y, Width * Position, Height, ActiveBarColor);
+
+                    if(KeepBoundaries) { // image limited to scroll size
+                        ScrollImage.SetPosition(
+                            (Width - ScrollImage.GetWidth()) * Position, (Height / 2) - (ScrollImage.GetWidth() / 2));
+                    } else {
+                        ScrollImage.SetPosition(
+                            Width * Position - ScrollImage.GetWidth() / 2, (Height / 2) - (ScrollImage.GetWidth() / 2));
+                    }
+                } else {
+                    painter.FillRectangle(
+                        ParentX + X, ParentY + Y + Height * (1.0 - Position), Width, ceil(Height * Position),
+                        ActiveBarColor);
+                    painter.FillRectangle(ParentX + X, ParentY + Y, Width, Height * (1.0 - Position), BarColor);
+
+                    if(KeepBoundaries) { // image limited to scroll size
+                        ScrollImage.SetPosition(
+                            (-ScrollImage.GetHeight() / 2) + (Width / 2),
+                            ((Height - ScrollImage.GetHeight())) * (1.0 - Position));
+                    } else {
+                        ScrollImage.SetPosition(
+                            (-ScrollImage.GetHeight() / 2) + (Width / 2),
+                            Height * (1.0 - Position) - ScrollImage.GetHeight() / 2);
+                    }
+                }
+
+                ScrollImage.Draw(
+                    painter, ParentX + X, ParentY + Y, ScrollImage.GetWidth() + Width, ScrollImage.GetHeight() + Height);
+            }
+        } else if(discrete == 1) {
+            uint8_t j = 0;
+            for(uint32_t i = MinValue; i <= MaxValue; i = i + step) {
+                uint16_t x = ParentX + X + j * (Width / division);
+                uint16_t y = ParentY + Y;
+                painter.DrawLine(x, ParentY + Y, x, y + Height, 0xFFFFFFFF);
+                char buffer[4];
+                snprintf(buffer, sizeof(buffer), "%d", j * step);
+                painter.DisplayAntialiasedString(SliderFont, x - 3, y - 35, buffer, 0xFFFFFFFF);
+                j++;
+            }
+
+            if(KeepBoundaries) { // image limited to scroll size
+                ScrollImage.SetPosition(
+                    (Width - ScrollImage.GetWidth()) * Position, (Height / 2) - (ScrollImage.GetWidth() / 2));
+            } else {
+                ScrollImage.SetPosition(
+                    Width * Position - ScrollImage.GetWidth() / 2, (Height / 2) - (ScrollImage.GetWidth() / 2));
+            }
+
+            ScrollImage.Draw(
+                painter, ParentX + X, ParentY + Y, ScrollImage.GetWidth() + Width, ScrollImage.GetHeight() + Height);
+        } else if(discrete == 2) {
+            for(uint16_t i = MinValue; i <= MaxValue; i++) {
+                float x = ParentX + X + (float)i * (float)Width / (float)MaxValue;
+                painter.DrawLine(x, Y, x, Y + Height, 0xFFFFFFFF);
+                // char buffer[10];
+                string buff;
+                if(MaxValue == 10) {
+                    if(i % 2 == 0) {
+                        buff = GetFriction(limiters[i]);
+                        // snprintf(buffer, sizeof(buffer), "%.3f", limiters[i]);
+                        painter.DisplayAntialiasedString(SliderFont, x - buff.size() * 2, Y - 35, buff.c_str(), 0xFFFFFFFF);
+                    }
+
+                } else {
+                    buff = GetFriction(limiters[i]);
+
+                    // snprintf(buffer, sizeof(buffer), "%c", buff);
+                    painter.DisplayAntialiasedString(SliderFont, x - buff.size() * 2, Y - 35, buff.c_str(), 0xFFFFFFFF);
+                }
+            }
+
+            if(KeepBoundaries) { // image limited to scroll size
+                ScrollImage.SetPosition(
+                    (Width - ScrollImage.GetWidth()) * Position, (Height / 2) - (ScrollImage.GetWidth() / 2));
+            } else {
+                ScrollImage.SetPosition(
+                    Width * Position - ScrollImage.GetWidth() / 2, (Height / 2) - (ScrollImage.GetWidth() / 2));
+            }
+
+            ScrollImage.Draw(
+                painter, ParentX + X, ParentY + Y, ScrollImage.GetWidth() + Width, ScrollImage.GetHeight() + Height);
+        }
+    }
+
+    void Slider::SetSelectedFrameColor(uint32_t color)
+    {
+        SelectedFrameColor = color;
+    }
+
+    uint32_t Slider::GetSelectedFrameColor() const
+    {
+        return SelectedFrameColor;
+    }
+
+    Slider& Slider::operator=(const Slider& Obj)
+    {
+        if(this != &Obj) {
+            Component::operator=(Obj);
+            ScrollImage = Obj.ScrollImage;
+            BarColor = Obj.BarColor;
+            ScrollColor = Obj.ScrollColor;
+            ActiveScrollColor = Obj.ActiveScrollColor;
+            FrameColor = Obj.FrameColor;
+            SelectedFrameColor = Obj.SelectedFrameColor;
+            ScrollLength = Obj.ScrollLength;
+            MinValue = Obj.MinValue;
+            MaxValue = Obj.MaxValue;
+            Position = Obj.Position;
+            Value = Obj.Value;
+            onValueChange = Obj.onValueChange;
+            KeepBoundaries = Obj.KeepBoundaries;
+        }
+
+        return *this;
+    }
+
+    bool Slider::IsTouchPointInObject(int32_t x, int32_t y, int32_t modificator)
+    {
+
+        if(!Visible) {
+            return false;
+        }
+
+        if(ScrollImage.IsEmpty()) {
+            return Component::IsTouchPointInObject(x, y, modificator);
+        }
+        if(Width > Height) { // Horizontal
+            int imgModificator = KeepBoundaries ? 0 : (ScrollImage.GetWidth() / 2);
+            return (bool)(x > X - modificator - imgModificator
+                            && x < (X + Width + modificator + imgModificator)
+                            && y > (Y + (Height / 2) - (ScrollImage.GetHeight() / 2) - modificator)
+                            && y < (Y + (Height / 2) + (ScrollImage.GetHeight() / 2) + modificator));
+        }
+        int imgModificator = KeepBoundaries ? 0 : (ScrollImage.GetHeight() / 2);
+        return (bool)(x > (X + (Width / 2) - (ScrollImage.GetWidth() / 2) - modificator)
+                        && x < (X + (Width / 2) + (ScrollImage.GetWidth() / 2) + modificator)
+                        && y > (Y - modificator - imgModificator)
+                        && y < (Y + Height + modificator + imgModificator));
+    }
+
+    void Slider::OnPress()
+    {
+        PreviousValueUpdateTimestamp = grvl::Callbacks()->get_timestamp();
+        Component::OnPress();
+    }
+
+    void Slider::OnRelease()
+    {
+        PreviousValueUpdateTimestamp = 0;
+        if(onValueChange.IsSet() && ReportedValue != Value) {
+            ReportedValue = Value;
+            Manager::GetInstance().GetEventsQueueInstance().push(&onValueChange);
+        }
+        Component::OnRelease();
+    }
+
+    void Slider::OnClick()
+    {
+        Component::OnClick();
+    }
+
+    Slider* Slider::BuildFromXML(XMLElement* xmlElement)
+    {
+        Manager* man = &Manager::GetInstance();
+
+        Slider* result = new Slider();
+
+        result->InitFromXML(xmlElement);
+
+        result->SetFrameColor(XMLSupport::ParseColor(xmlElement, "frameColor", (uint32_t)COLOR_ARGB8888_TRANSPARENT));
+        result->SetSelectedFrameColor(XMLSupport::ParseColor(xmlElement, "selectedFrameColor", (uint32_t)result->GetFrameColor()));
+        result->SetBarColor(XMLSupport::ParseColor(xmlElement, "barColor", (uint32_t)COLOR_ARGB8888_WHITE));
+        result->SetActiveBarColor(XMLSupport::ParseColor(xmlElement, "activeBarColor", (uint32_t)result->GetBarColor()));
+        result->SetScrollColor(XMLSupport::ParseColor(xmlElement, "scrollColor", (uint32_t)COLOR_ARGB8888_BLUE));
+        result->SetActiveScrollColor(XMLSupport::ParseColor(xmlElement, "activeScrollColor", (uint32_t)result->GetScrollColor()));
+
+        result->SetMaxValue(XMLSupport::GetAttributeOrDefault(xmlElement, "maxValue", (uint32_t)100));
+        result->SetMinValue(XMLSupport::GetAttributeOrDefault(xmlElement, "minValue", (uint32_t)0));
+
+        result->SetKeepBoundaries(XMLSupport::GetAttributeOrDefault(xmlElement, "keepBoundaries", false));
+
+        result->SetDiscrete(XMLSupport::GetAttributeOrDefault(xmlElement, "isDiscrete", (uint32_t)0));
+
+        result->SetDivision(XMLSupport::GetAttributeOrDefault(xmlElement, "division", (uint32_t)0));
+
+        result->SetTextFont(man->GetFontPointer(XMLSupport::GetAttributeOrDefault(xmlElement, "font", "normal")));
+
+        const char* tempChar = xmlElement->Attribute("image");
+        if(tempChar) {
+            result->SetScrollImage(Image());
+            man->BindImageContentToImage(tempChar, result->GetScrollImagePointer());
+        }
+
+        result->SetOnValueChangeEvent(man->GetEventWithArguments(xmlElement->Attribute("onValueChange")));
+
+        return result;
+    }
+
+    Touch::TouchResponse Slider::ProcessMove(int32_t StartX, int32_t StartY, int32_t DeltaX, int32_t DeltaY)
+    {
+        float Offset = 0;
+        if(!ScrollImage.IsEmpty()) {
+            if(Width > Height) {
+                Offset = KeepBoundaries ? (ScrollImage.GetWidth() / 2) : 0;
+            } else {
+                Offset = KeepBoundaries ? (ScrollImage.GetHeight() / 2) : 0;
+            }
+        } else
+            Offset = ScrollLength;
+
+        float tempVal = 0;
+
+        if(Width > Height) {
+            tempVal = PositionToValue(((float)(StartX + DeltaX - Offset)) / ((float)Width - 2 * Offset));
+        } else {
+            tempVal = PositionToValue((float)((Height - Offset) - (StartY + DeltaY)) / ((float)Height - 2 * Offset));
+        }
+
+        if(tempVal > MaxValue) {
+            tempVal = MaxValue;
+        } else if(tempVal < MinValue) {
+            tempVal = MinValue;
+        }
+
+        Position = ValueToPosition(tempVal);
+        Value = tempVal;
+
+        if(onValueChange.IsSet() && Value != ReportedValue && PreviousValueUpdateTimestamp < (grvl::Callbacks()->get_timestamp() - 400)) { // Report time
+            PreviousValueUpdateTimestamp = grvl::Callbacks()->get_timestamp();
+            ReportedValue = Value;
+            Manager::GetInstance().GetEventsQueueInstance().push(&onValueChange);
+        }
+
+        return Touch::TouchHandled;
+    }
+
+    float Slider::ValueToPosition(float value) const
+    {
+        return (value - MinValue) / (MaxValue - MinValue);
+    }
+
+    float Slider::PositionToValue(float position) const
+    {
+        if(discrete == 1) {
+            float range = MaxValue - MinValue;
+            float newValue = range * position;
+            int val = newValue / step;
+            newValue = val * step;
+            return newValue + MinValue;
+        }
+        if(discrete == 0) {
+            float range = MaxValue - MinValue;
+            float newValue = range * position;
+            return newValue + MinValue;
+        }
+        if(discrete == 2) {
+            float range = MaxValue - MinValue;
+            float newValue = (int)roundf(range * position);
+            return newValue + MinValue;
+        }
+    }
+
+    void Slider::SetOnValueChangeEvent(const Event& event)
+    {
+        onValueChange = event;
+        onValueChange.SetSenderPointer(this);
+    }
+
+    void Slider::SetActiveBarColor(uint32_t color)
+    {
+        ActiveBarColor = color;
+    }
+
+    uint32_t Slider::GetActiveBarColor() const
+    {
+        return ActiveBarColor;
+    }
+
+    void Slider::SetKeepBoundaries(bool value)
+    {
+        KeepBoundaries = value;
+    }
+
+    bool Slider::GetKeepBoundaries() const
+    {
+        return KeepBoundaries;
+    }
+
+} /* namespace grvl */
+
+//NOLINTEND(readability-magic-numbers, bugprone-branch-clone, bugprone-integer-division)
