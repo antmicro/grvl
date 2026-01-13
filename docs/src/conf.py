f8383804bb194d5d3c7552a4cdaf72cb2701675b
Kacper Zienkiewicz <kzienkiewicz@internships.antmicro.com>
2026-01-13 17:08:02 +0100

    [#89813] docs: conf: Provide gh_slug to antmicro_html

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -35,7 +35,7 @@ html_show_sphinx = False
     html_logo,
     html_theme_options,
     html_context
-) = antmicro_html(pdf_url=f"{basic_filename}.pdf")
+) = antmicro_html(gh_slug='antmicro/grvl', pdf_url=f"{basic_filename}.pdf")
 html_title = project
 (
     latex_elements,
